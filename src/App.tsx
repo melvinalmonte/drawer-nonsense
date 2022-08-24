f3443883a9d6df668041e9ded7ea0bb32289a426
Melvin A <melvinalmonte@gmail.com>
2022-08-23 23:21:45 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import ClippedDrawerRight from "./ClippedDrawerRight";
+
+function App() {
+  return (
+    <>
+      <nav style={{ position: "sticky", top: 0 }}>hello header</nav>
+
+      <ClippedDrawerRight />
+    </>
+  );
+}
+
+export default App;
